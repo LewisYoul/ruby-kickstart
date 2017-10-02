87d04dddf1f18db862bd44b6e79509154a147649
Lewis Youl <lewisyoul@gmail.com>
2017-10-02 17:16:02 +0100

    added session 2 challenge 6 & 7

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -1,16 +1,32 @@
 # Write a method named prime_chars? which takes array of strings
-# and returns true if the sum of the characters is prime. 
-# 
-# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself. 
-# 
+# and returns true if the sum of the characters is prime.
+#
+# Remember that a number is prime if the only integers that can divide it with no remainder are 1 and itself.
+#
 # Examples of length three
 # prime_chars? ['abc']            # => true
 # prime_chars? ['a', 'bc']        # => true
 # prime_chars? ['ab', 'c']        # => true
-# prime_chars? ['a', 'b', 'c']    # => true
-# 
+# prime_chars? C    # => true
+#
 # Examples of length four
 # prime_chars? ['abcd']           # => false
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+def prime_chars? arr
+  num = arr.join.length
+  half = num/2.to_i
+  return false if num < 2
+  2.upto(half) { |n|
+  if num % n == 0
+    false
+  end
+  }
+  true
+end
+
+puts prime_chars? ['ab', 'cd']
+
+#I can not work this one out!
--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,18 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(str)
+  rtn = []
+  str.gsub(/[?|\!@<>$#%^&*(\[\])-=_+:;,.]/, ' ').split(' ').each_with_index { |v, i|
+  rtn << v if i % 2 == 0
+  }
+rtn
+end
+
+#puts alternate_words "If you want to accomplish something in the world, idealism is not enough--you need to choose a method that works to achieve the goal. In other words, you need to be pragmatic."
