735c3c26c91a71f1729ef9f673ca7bdfe3025865
Lewis Youl <lewisyoul@gmail.com>
2017-10-03 16:49:55 +0100

    added session2 challenge12

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -1,13 +1,13 @@
 # Define a class called Fraction
 # This class is used to represent a ratio of two integers
 # Include setter methods called numerator and denominator that allow the user to change these values
-# Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
-# Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
-# are reduced to lowest terms (ie 20/60 becomes 1/3)
-# This will require finding the greatest common divisor for the numerator and denominator
-# then dividing both by that number.
-# I have included a greatest common divisor method for you
-# You should also define a method, to_s, that will represent the Fraction as a String
+ #Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
+ #Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
+ #are reduced to lowest terms (ie 20/60 becomes 1/3)
+ #This will require finding the greatest common divisor for the numerator and denominator
+ #then dividing both by that number.
+ #I have included a greatest common divisor method for you
+ #You should also define a method, to_s, that will represent the Fraction as a String
 #
 # EXAMPLE:
 # f = Fraction.new 20, 60
@@ -21,8 +21,31 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  attr_accessor 'numerator', 'denominator'
+
+  def initialize (numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+
+  def lowest
+    Fraction.new (@numerator/gcd(@numerator, @denominator)), (@denominator/gcd(@numerator, @denominator))
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+
 end
+
+#f = Fraction.new 50, 100
+#puts f.to_s
