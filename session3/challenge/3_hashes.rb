343466b3e21a0b8184c8dbb48fe3c6c55603207f
Lewis Youl <lewisyoul@gmail.com>
2017-10-05 13:27:12 +0100

    session3 challenge3

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,19 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(str)
+
+  rtn_hash = Hash.new { |hash, key| hash[key] = 1 }
+
+  str.downcase.split(' ').each do |v|
+    if rtn_hash.has_key? v
+      rtn_hash[v] += 1
+    else
+      rtn_hash[v]
+    end
+  end
+  rtn_hash
+end
+
+#puts word_count "split me me up up up"
