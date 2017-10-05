90394159926c3b65104f7813c12aff2193916c6d
Lewis Youl <lewisyoul@gmail.com>
2017-10-05 14:56:15 +0100

    session3 challenge4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,20 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str)
+  rtn_hash = {}
+
+  arr = str.split(' ')
+
+  arr.each_with_index do |e, i|
+    if rtn_hash.has_key? e
+      true
+    else
+      rtn_hash[e] = i
+    end
+  end
+  rtn_hash
+end
+
+#puts first_pos "zero one two three four four four seven"
