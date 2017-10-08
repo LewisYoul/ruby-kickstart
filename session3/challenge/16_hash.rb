fdadeb8801d269e47f8b9d422a1021f050cea0c3
Lewis Youl <lewisyoul@gmail.com>
2017-10-08 14:52:28 +0100

    session3 challenges 16 and 17

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,24 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+
+head = {:data => 1, :next => nil}
+head = {:data => 2, :next => head}
+head = {:data => 3, :next => head}
+
+puts head[:next]
+
+
+
+#in this method we want to continue printing out the contents of :data until
+#there is no data left to be printed (the base :next is set to nil)
+#the boolean value of nil is false
+def print_list(list)
+  while list
+    puts list[:data] #print out the value assigned to the data key for each node in the list
+    list = list[:next] #assign the next node to the list variables
+  end                   #this means that the next time list[:data] is printed the next value down the node will be puts to stdout
+end
+
+
+print_list head
--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,19 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+=begin
+head = {:data => 1, :next => nil}
+head = {:data => 2, :next => head}
+head = {:data => 3, :next => head}
+=end
+
+def print_list_in_reverse(list)
+  rtn_array = []
+  while list
+    rtn_array << list[:data]
+    list = list[:next]
+  end
+  rtn_array.reverse.each { |n| puts n }
+end
+
+#print_list_in_reverse head
